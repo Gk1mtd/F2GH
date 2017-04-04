5da60d534d7e5685f27bd968a1cec8b900c00cbb
Michael <arvi_1@gmx.de>
2017-04-04 19:06:58 +0200

    - added player shooting, rotating, bullets

--- F2GH/Assets/Scripts/Bullet.cs
+++ F2GH/Assets/Scripts/Bullet.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Entity : MonoBehaviour {
-    [SerializeField]
-    protected int life;
-    [SerializeField]
-    protected float speed;
+public class Bullet : Entity {
 
     protected Vector2 position;
 
     // Use this for initialization
     void Start () {
-		
+        speed = 5;
 	}
 	
 	// Update is called once per frame
--- F2GH/Assets/Scripts/Player.cs
+++ F2GH/Assets/Scripts/Player.cs
@@ -4,18 +4,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : Entity {
-    
-	// Use this for initialization
-	void Start () {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    // Use this for initialization
+    void Start () {
         life = 10;
+        speed = 5.0f;
+        position = new Vector2(0, 0);
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+        meth_movement();
+        meth_shoot();
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+        Debug.Log("Got hit by something");
+    }
     private void meth_shoot()
     {
-        Debug.Log("I´ve fired!");
+        if (Input.GetAxis("TriggerRight") > 0.0f)
+        {
+            var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+            // Add velocity to the bullet
+            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.up * 6;
+
+            // Destroy the bullet after 2 seconds
+            Destroy(bullet, 2.0f);
+        }
     }
+    private void meth_movement()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        var x2 = -Input.GetAxis("HorizontalRight");
+        var y2 = Input.GetAxis("VerticalRight");
+
+        if (x2 != 0 || y2 != 0)
+        {
+            float heading = Mathf.Atan2(x2, y2);
+            transform.rotation = Quaternion.Euler(0f, 0f, heading * Mathf.Rad2Deg);
+        }
+        if (x != 0 || y != 0)
+        {
+            position = position + new Vector2(x, y);
+            transform.position = position;
+        }
+    }
+
 }
