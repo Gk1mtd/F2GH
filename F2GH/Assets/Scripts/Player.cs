a575340eeef1b7d81594e81ac2dbe145b62a5d05
Michael <arvi_1@gmx.de>
2017-04-04 14:58:42 +0200

    - added player movement

---
+++ F2GH/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity {
+    
+	// Use this for initialization
+	void Start () {
+        life = 10;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    private void meth_shoot()
+    {
+        Debug.Log("I´ve fired!");
+    }
+}
